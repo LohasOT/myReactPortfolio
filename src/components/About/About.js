ae5766b86e8508d3f65e865d23b4be8db131c27f
LohasOT <oliverhoang93@gmail.com>
2022-01-27 11:20:04 -0800

    updated carolsel

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -17,24 +17,28 @@ const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 const images = [
   {
-    label: 'San Francisco – Oakland Bay Bridge, United States',
+    label: 'San Francisco, United States',
     imgPath:
       'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+    link: 'https://www.google.com',
   },
   {
     label: 'Bird',
     imgPath:
       'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+    link: 'https://www.google.com',
   },
   {
     label: 'Bali, Indonesia',
     imgPath:
       'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80',
+    link: 'https://www.google.com',
   },
   {
     label: 'Goč, Serbia',
     imgPath:
       'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
+    link: 'https://www.google.com',
   },
 ];
 
@@ -86,6 +90,7 @@ const About = () => {
           {images.map((step, index) => (
             <div key={step.label}>
               {Math.abs(activeStep - index) <= 2 ? (
+                <Button href={step.link} target="_blank" rel="noopener noreferrer">
                 <Box
                   component="img"
                   sx={{
@@ -98,6 +103,7 @@ const About = () => {
                   src={step.imgPath}
                   alt={step.label}
                 />
+                </Button>
               ) : null}
             </div>
           ))}
