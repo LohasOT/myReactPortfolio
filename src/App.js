160ce5946b6c669929dc6ad86e58a8de6565d889
LohasOT <oliverhoang93@gmail.com>
2022-01-27 00:37:24 -0800

    updated nav and foot

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div>
-    </div>
+    <>
+    <Navbar>
+      </Navbar>
+    <Footer>
+    </Footer>
+    </>
   );
 }
 
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,31 +1,20 @@
 import React from 'react';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import './Footer.css';
 
-function Footer() {
+const Footer = () => {
 
   // Replace links with social media profiles
-  const icons = [
-    {
-      name: "fab fa-github",
-      link: "https://github.com/"
-    },
-    {
-      name: "fab fa-linkedin",
-      link: "https://www.linkedin.com/"
-    },
-    {
-      name: "fab fa-stack-overflow",
-      link: "https://stackoverflow.com/"
-    }
-  ]
 
   return (
-    <footer className="flex-row px-1">
-      {icons.map(icon =>
-      (
-        <a href={icon.link} key={icon.name} target="_blank" rel="noopener noreferrer"><i className={icon.name}></i></a>
-      )
-      )}
+    <>
+    <footer id="foot" className="flex-row px-1">
+        <a href="https://github.com/Abraham-Solis" rel="noopener noreferrer" target="_blank"><img src="https://img.icons8.com/nolan/96/github.png" alt="gitHub" /></a>
+        <a href="https://www.linkedin.com/in/abraham-solis-b08bb3194/" rel="noopener noreferrer" target="_blank"><img src="https://img.icons8.com/nolan/96/linkedin.png" alt='link' /></a>
     </footer>
+    </>
+
   );
 }
 
