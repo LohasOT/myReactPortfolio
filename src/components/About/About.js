6cbac94f386bf17a46f768ea43c6c0232f0c25b9
LohasOT <oliverhoang93@gmail.com>
2022-01-28 10:41:25 -0800

    update css

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -16,60 +16,8 @@ import Grid from '@mui/material/Grid';
 import { styled } from '@mui/material/styles';
 import "./About.css";
 
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
-
-const images = [
-  {
-    label: 'San Francisco, United States',
-    imgPath:
-      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
-    link: 'https://www.google.com',
-  },
-  {
-    label: 'Bird',
-    imgPath:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
-    link: 'https://www.google.com',
-  },
-  {
-    label: 'Bali, Indonesia',
-    imgPath:
-      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80',
-    link: 'https://www.google.com',
-  },
-  {
-    label: 'Goč, Serbia',
-    imgPath:
-      'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
-    link: 'https://www.google.com',
-  },
-];
-
 const About = () => {
   
-  const theme = useTheme();
-  const [activeStep, setActiveStep] = React.useState(0);
-  const maxSteps = images.length;
-
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const handleStepChange = (step) => {
-    setActiveStep(step);
-  };
-
-  const Item = styled(Paper)(({ theme }) => ({
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }));
-
   return (
     <>
     <Container className="opening">
@@ -82,21 +30,12 @@ const About = () => {
     </Container>
     <Container className="empty"></Container>
     <Container className="about">
-        <h1>1. About Me <hr></hr></h1>
-        <Grid className="about" container spacing={2}>
-          <Grid className="about" item xs={5} md={5}>
-            <Item className="about">
-              <main xs={5}>
+        <h1 className='title'>1. About Me <hr></hr></h1>
+              <main>
               <h2>I am an experienced coding bootcamp graduate with extensive background in Javascript, HTMLContainer, CSS, developing mobile friendly web applications, and building websites from the ground up. Professional strengths include creative problem-solving, written and verbal communication, and time management.</h2>
             </main>
-            </Item >
-          </Grid>
-          <Grid className="about" item xs={7} md={7}>
-            <Item className="about">
-              <img src={myPicture} xs={7} md={7} />
-            </Item>
-          </Grid>
-        </Grid>
+        <img className="picture" src={myPicture} alt={myPicture}/>
+
       </Container>
       <br></br>
       <br></br>
