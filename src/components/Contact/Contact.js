68371ef88bf8c0501c0aff7f798f03ce6c516152
LohasOT <oliverhoang93@gmail.com>
2022-01-29 11:36:29 -0800

    finished contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,21 +8,14 @@ const Contact = () => {
     <h1 className='title3'>3. What's Next?</h1>
     <h1 className='miniTitle'>Let's Get In Touch <hr></hr></h1>
     <br></br>
-    <Row>
-    <Col md="4"></Col>
-    <Col md="3">
+    <div className='contactBox'>
     <h3 className='contactInfo'>I’m currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!</h3>
-    </Col>
-    <Col md="4"></Col>
-    </Row>
+    </div>
     <br></br>
-    <Row>
-    <Col xs="4" md="5"></Col>
-    <Col className='buttonBox' xs="3" md="2">
-    <Button className='button' onClick={() => window.location = 'mailto:oliverhoang93@gmail.com'}>ContactMe</Button>
-    </Col>
-    <Col xs="5" md="5"></Col>
-    </Row>
+    <div className='buttonBox'>
+    <Button className='button' onClick={() => window.location = 'mailto:oliverhoang93@gmail.com'}>Contact Me</Button>
+    </div>
+      <br></br><br></br><br></br>
     </>
   );
 }
