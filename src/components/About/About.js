0bff720346633363c6537bb98eae410ec1840682
LohasOT <oliverhoang93@gmail.com>
2022-01-28 22:58:50 -0800

    fixed css

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -18,10 +18,11 @@ const About = () => {
     </div>
     <div className="empty"></div>
     </Container>
+      <br></br><br></br><br></br><br></br>
     <Container fluid className="aboutMe">
     <Row>
       <h1 className='title'>1. About Me <hr></hr></h1>
-    <Col xs={12} md={4}>
+    <Col xs={12} md={5}>
             <main className="about">
               <h2 className="para">I am an experienced coding bootcamp graduate with extensive background in Javascript, HTMLContainer, CSS, developing mobile friendly web applications, and building websites from the ground up.</h2>
               <h2 className="para">Professional strengths include creative problem-solving, written and verbal communication, and time management.</h2>
@@ -31,16 +32,16 @@ const About = () => {
               <li>React</li>
               <li>Node.js</li>
       </Col>
-    <Col xs={0} md={4}></Col>
-    <Col xs={6} md={4}>
-      <br></br>
+    <Col xs={0} md={2}></Col>
+    <Col xs={6} md={5}>
       <br></br>
         <Image className="picture" src={myPicture} alt= {myPicture} />
       </Col>
     </Row>
     </Container>
-      <br></br>
-      <br></br>
+      <br></br>      <br></br>      <br></br>      <br></br>      <br></br>      <br></br>      <br></br>      <br></br>
+
+
 
     </>
   );
